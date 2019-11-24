d7562a718e7c57fb328b6d1bd1b0a26414fcedac
Maik Müller <mueller@media-it-services.de>
2019-11-24 02:23:10 +0100

    just save, doesn't work currently!

--- src/resources/views/menus/partials/edit/content.blade.php
+++ src/resources/views/menus/partials/edit/content.blade.php
@@ -1,4 +1,4 @@
-<article class="panel is-primary">
+<article class="panel is-primary" >
 
     <p class="panel-heading is-capitalized">
 
@@ -7,10 +7,36 @@
     </p>
 
 
-    <div class="panel-block" style="width: 100%">
+    <div class="panel-block" style="margin-top: 60px">
 
         <form action="{{ route('y_menus.update', ['menu' => $menu]) }}" method="POST">
 
+            @csrf
+            @method('patch')
+
+            @include('yabe::components.input_text', [
+                'name' => 'name',
+                'value' => $menu->name,
+                'iconClass' => "fas fa-bars",
+                'label' => __('yabe::words.name')
+
+            ])
+
+            @include('yabe::components.input_text', [
+                'name' => 'title',
+                'value' => $menu->title,
+                'iconClass' => "fas fa-bars",
+                'label' => __('yabe::words.title')
+
+            ])
+
+            @include('yabe::components.input_text', [
+                'name' => 'url',
+                'value' => $menu->url,
+                'iconClass' => "fas fa-bars",
+                'label' => __('yabe::words.url')
+
+            ])
 
 
         </form>
